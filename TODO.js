5b66182576d1a236f8def782f820952811b25a8b
7 <a7.@7s-MacBook-Pro.local>
2022-06-29 22:25:05 +0300

    added new file readme.md

---
+++ TODO.js
@@ -0,0 +1,57 @@
+/* TODO: 1.Display the numbers in the range 0...50 that are multiples of 5. */
+
+
+
+/* TODO: 2. Change the following function into arrow function. */
+function checkUser(user){
+ if(!user){
+    return console.log("No value");
+ } else{
+    return console.log(welcome)
+ }
+}
+
+checkUser();
+
+
+
+/* TODO: 3. Complete the following code to get the extension of a filename. */
+filename = "index.html"
+console.log();
+filename = "index.js"
+console.log();
+
+
+
+
+/* TODO: 4. Write a function that convert a given number to hours and minutes. */
+
+
+
+
+/* TODO: 5. Write a function that prints the most frequent element in the following array */
+var arr = [];
+for(var i = 0; i < 10; i++){
+    arr[i] = Math.floor(Math.random()* 5);
+}
+console.log(arr);
+
+
+
+/* TODO: 6. Write a function that takes a string and returns the first integer in the string. 
+         If the string does not contain an integer. a message will be displayed.*/
+
+
+
+
+
+/* TODO: 7. Write a function that takes 2 random integers and displays the message "Taibah valley"
+            if the reminder of both numbers is a product of 3, "Summer training 2022" if the reminder
+            is a product of 5 and both messeges otherwise. */
+
+
+
+/* TODO: 8. Write a function that calculate the Age and return it. */
+
+
+
